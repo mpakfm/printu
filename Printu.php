985826f6f08b1d22185f81c26ed226d7c6a3caf6
Sergey Fomin <mpakfm@gmail.com>
2020-07-02 00:11:09 +0300

    New version 1.2 new style

--- Printu.php
+++ Printu.php
@@ -3,7 +3,7 @@
  * Simple Logger
  * @author mpakfm <mpakfm@google.com>
  * @package \Mpakfm\Printu
- * @version 1.1.3
+ * @version 1.2
  */
 
 namespace Mpakfm;
@@ -12,9 +12,126 @@ class Printu {
 
     /**
      * Path to log folder
-     * @var string 
+     * @var string
      */
     public static $logPath;
+
+    /**
+     * @var mixed
+     */
+    private $obj;
+    /**
+     * @var string
+     */
+    private $title;
+
+    /**
+     * @var mixed
+     */
+    private $response = 'text';
+
+    /**
+     * @var \DateTime
+     */
+    private $dt;
+
+    /**
+     * @var string
+     */
+    private $dtFormat = 'd.m H:i:s';
+
+    /**
+     * @var string
+     */
+    public $file;
+
+    public function __construct($obj) {
+        if ($obj === true) {
+            $obj = 'TRUE (bool)';
+        }
+        if ($obj === false) {
+            $obj = 'FALSE (bool)';
+        }
+        if (is_null($obj)) {
+            $obj = 'NULL';
+        }
+        if (is_string($obj) && $obj == '') {
+            $obj = 'EMPTY LINE (string)';
+        }
+        $this->obj = $obj;
+    }
+
+    public function title(string $title) {
+        $this->title = $title;
+        return $this;
+    }
+
+    public function dt(\DateTime $dt = null) {
+        if ($dt) {
+            $this->dt = $dt;
+        } else {
+            $this->dt = new \DateTimeImmutable();
+        }
+        return $this;
+    }
+
+    public function timeFormat(string $format) {
+        $this->dtFormat = $format;
+        return $this;
+    }
+
+    /**
+     * @param $response mixed
+     */
+    public function response($response) {
+        $this->response = $response;
+        return $this;
+    }
+
+    public function file(string $file) {
+        $this->file = $file;
+        if (strpos($this->file, '.') === false) {
+            $this->file .= '.log';
+        }
+        return $this;
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function show() {
+        $string = '';
+        if ($this->dt) {
+            $string .= $this->dt->format($this->dtFormat)."\t";
+        }
+        if ($this->title) {
+            $string .= ($this->title == '' ? '': "{$this->title}: ");
+        }
+        $string .= print_r($this->obj, true)."\n";
+        if ($this->response === true) {
+            return $string;
+        } else {
+            switch ($this->response) {
+                case"file":
+                    if (!$this->file) {
+                        $this->file = 'info.log';
+                    }
+                    $filePath = static::$logPath . DIRECTORY_SEPARATOR . $this->file;
+                    if (static::$logPath) {
+                        file_put_contents($filePath, $string, FILE_APPEND);
+                        return $filePath;
+                    } else {
+                        throw new \Exception ('Printu cannot create this log file: ' . $filePath . '. You need check var $pathToLogFolder ("' . static::$logPath . '") in init line: \Mpakfm\Printu::setPath($pathToLogFolder).');
+                    }
+                    break;
+                case"text":
+                    echo $string;
+                    break;
+                default:
+                    echo '<div style="color: #000; text-align:left; background-color:#FFFAFA; border: 1px solid silver; margin: 10px 10px 10px 10px; padding: 10px 10px 10px 10px;">',$this->title == '' ? '': "<b>{$this->title}:&nbsp;</b>", nl2br(str_replace([' ','<','>'], ['&nbsp;','&lt;','&gt;'], print_r($this->obj,true))),'</div>';
+            }
+        }
+    }
 
     /**
      * Set path to log folder
@@ -32,17 +149,22 @@ class Printu {
         return true;
     }
 
+    public static function obj($obj): Printu {
+        return new self($obj);
+    }
+
     /**
      * Make log to screen or file
-     * return: 
+     * return:
      *      true - return string
      *      file - print into file: static::$logPath . '/{$file}'
      *      text|ajax - print in plain text
      *      false - print in html
      * @param mixed $obj
      * @param string $title
-     * @param string $return 
+     * @param string $return
      * @param string $file
+     * @throws \Exception
      * @return mixed (string | boolean | void)
      */
     public static function log($obj, string $title='', $return=null, string $file=null) {
@@ -58,7 +180,7 @@ class Printu {
         if (is_string($obj) && $obj == '') {
             $obj = 'EMPTY LINE (string)';
         }
-        $string = ($title == '' ? '': "$title: ") .print_r($obj, true)."\n";		
+        $string = ($title == '' ? '': "$title: ") .print_r($obj, true)."\n";
         if ($return === true) {
             return $string;
         } elseif ($return == 'file') {
@@ -75,7 +197,8 @@ class Printu {
         } elseif ($return == 'text' || $return == 'ajax') {
             echo $string;
         } else {
-            echo '<div align="left" style="color: #000; text-align:left; background-color:#FFFAFA; border: 1px solid silver; margin: 10px 10px 10px 10px; padding: 10px 10px 10px 10px;">',$title == '' ? '': "<b>{$title}:&nbsp;</b>", nl2br(str_replace([' ','<','>'], ['&nbsp;','&lt;','&gt;'], print_r($obj,true))),'</div>';
+            echo '<div style="color: #000; text-align:left; background-color:#FFFAFA; border: 1px solid silver; margin: 10px 10px 10px 10px; padding: 10px 10px 10px 10px;">',$title == '' ? '': "<b>{$title}:&nbsp;</b>", nl2br(str_replace([' ','<','>'], ['&nbsp;','&lt;','&gt;'], print_r($obj,true))),'</div>';
         }
+        return;
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # printu
 # Simple Logger
+
+version 1.2
+
+New samples:
+```php
+Printu::setPath(__DIR__ . '/var/log');
+
+Printu::obj('-----')->response('file')->show();
+
+$dt = new \DateTime();
+$dt->sub(new \DateInterval('P1MT10H'));
+Printu::obj('test')->title('IndexController::index')->dt($dt)->response('file')->show();
+
+Printu::obj('test H:i:s in log file info.log')->dt()->timeFormat('H:i:s')->response('file')->show();
+
+Printu::obj('test in log file error.log')->response('file')->file('error')->show();
+
+Printu::obj('test echo')->show();
+```
+
+tail -f var/log/info.log:
+```log
+-----
+01.06 13:50:13  IndexController::index: test
+23:31:08        test H:i:s in log file info.log
+```
+tail -f var/log/error.log:
+```log
+-----
+test in log file error.log
+```
+
+Old version samples:
+```php
+Printu::setPath(__DIR__ . '/var/log');
+
+$dt = new \DateTime();
+$dt->sub(new \DateInterval('P1MT10H'));
+Printu::log('-------', '', 'file');
+Printu::log('test', $dt->format('d.m H:i:s')."\t".'IndexController::index', 'file');
+Printu::log('test in log file info.log', 'IndexController::index', 'file');
+Printu::log('test in log file error.log', 'IndexController::index', 'file', 'error.log');
+```
+tail -f var/log/info.log:
+```log
+-------
+01.06 13:58:20  IndexController::index: test
+IndexController::index: test in log file info.log
+```
+tail -f var/log/error.log:
+```log
+IndexController::index: test in log file error.log
+```
